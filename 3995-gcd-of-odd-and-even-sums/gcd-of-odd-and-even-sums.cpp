f1bb1f9f78f769469f22e7fdc9cd4027bdff2bc9
AbdulRahman <abdulrahman21062004@gmail.com>
2026-07-15 19:20:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.40%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+        // return n;
+       int sumodd=1;
+       int sumeven=2;
+        for(int i=1; i<n; i++){
+            sumodd += 2;
+        }
+        int a = sumodd-2;
+        while(a>0){
+            sumodd += a;
+            a -= 2;
+        }
+        for(int i=1; i<n; i++){
+            sumeven += 2;
+        }
+        int b = sumeven-2;
+        while(b>0){
+            sumeven += b;
+            b -= 2;
+        }
+        
+         return gcd(sumodd, sumeven);
     }
 };
